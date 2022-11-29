5f81481bb905ce600677d4b7e054de7464ffb311
Henry Wu <hhw67865@gmail.com>
2022-11-29 11:22:12 -0500

    commit tree eb9387c245362b25ee43dc4e67f8ce049af42db1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,45 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(a)
+        self.create(title: a)
+    end
 
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id id
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes (yes)
+        self.find_by(yes)
+    end
+
+    def self.find_movies_after_2002
+        self.where(["release_date > ?",2002])
+    end
+
+    def update_with_attributes (x)
+        self.update(x)
+    end
+    
+    def self.update_all_titles (a)
+        self.update(title: a)
+    end
+
+    def self.delete_by_id (a)
+        self.destroy(a)
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
 end
